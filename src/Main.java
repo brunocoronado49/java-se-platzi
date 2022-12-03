8f3267e61ddb78fd6e699fe03a1aae15272f93a7
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-12-03 15:22:00 -0600

    Iniciando con el curso de Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int age = 25;
+        String name = "Bruce";
+        boolean work;
+        work = true;
+        System.out.println("Hola soy " + name  + " y tengo " + age + "años");
+        System.out.println(work);
+    }
+}
