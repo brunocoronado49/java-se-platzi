b0d1a018c228d427b8f11a0a32525590f2e00cb2
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-12-03 16:01:17 -0600

    Tipos de datos numericos

---
+++ src/NamingJava.java
@@ -0,0 +1,30 @@
+// Upper Camel Case
+public class NamingJava {
+    public static void main(String[] args) {
+        // Manera de nommbrar a variables validas
+
+        int celphone = 492210588;
+        int celPhone = 458734874;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $name = "Franco";
+        String _name = "Bruce";
+        System.out.println($name);
+        System.out.println(_name);
+
+        String currency$ = "MXN";
+        String back_color = "blue";
+        System.out.println(currency$);
+        System.out.println(back_color);
+
+        // Constantes
+        int POSITION = 5;
+        int MAX_WIDTH = 98;
+        System.out.println(POSITION);
+        System.out.println(MAX_WIDTH);
+
+        // Lower camel case
+        String fullName = "Francisco Rangel";
+    }
+}
